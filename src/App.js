58f81d4fa862544000350d1054b140a52bc42547
Ambati Avinash <avi.ambati07@gmail.com>
2020-03-09 16:17:13 +0530

    incrementing age using react redux

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { connect } from 'react-redux';
+
 import UserInput from './Components/UserInput';
 import UserOutput from './Components/UserOutput';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      username : 'Avinash',
-      age : 25
-    }
-  }
-
-//   nameChangeHandler = (e) => {
-//     this.setState({
-//         username: e.target.value
-//     })
-// }
-
-// ageUp = () => {
-//   this.setState({
-//     age: ++this.state.age
-//   })
-// }
-
-// ageDown = () => {
-//   this.setState({
-//     age: --this.state.age
-//   })
-// }
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     username : 'Avinash',
+  //     age : 25
+  //   }
+  // }
 
   render(){
     return (
@@ -41,12 +25,25 @@ class App extends Component {
         />
         <UserOutput name = {this.state.username}/> */}
 
-        <div>Age: <span> {this.state.age}</span></div>
-        <button onClick={this.ageUp}>Age Up </button>
-        <button onClick={this.ageDown}>Age Down</button>
+        <div>Age: <span> {this.props.age}</span></div>
+        <button onClick={this.props.onAgeUp}>Age Up </button>
+        <button onClick={this.props.onAgeDown}>Age Down</button>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    return{
+      age: state.age
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+      onAgeUp: () => dispatch({type: 'ADD'}),
+      onAgeDown: () => dispatch({type: 'SUBTRACT'})
+    }  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -5,6 +5,13 @@ const initialState = {
 const reducer = (state=initialState, action) => {
     const newState = {...state};
 
+    if(action.type === 'ADD'){
+        newState.age += 1
+    }
+    if(action.type === 'SUBTRACT'){
+        newState.age -= 1
+    }
+
     return newState;
 }
 
